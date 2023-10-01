2387ce38d9bfaf1d61193d1c85ffacfb87b186ba
apidemo <deepakkumarnaik3@gmail.com>
2023-10-01 21:35:12 +0530

    add anotation

--- Example/src/main/java/com/example/Example/Entity/UserEntity.java
+++ Example/src/main/java/com/example/Example/Entity/UserEntity.java
@@ -1,14 +1,27 @@
 package com.example.Example.Entity;
-
-import lombok.Data;
-
+import lombok.*;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long UId;
+
     private String Name;
+
     private String Email;
+
     private String Password;
 
 }
